e0cdfbca7e4e07ba1c62ad616b146428b4e6bd39
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-09-19 12:25:32 +0430

    main.c file added

---
+++ main.c
@@ -0,0 +1,36 @@
+/*
+ *              In The Name of God 
+ * 
+ * ==========================================
+ * [] File Name:  main.c
+ * 
+ * [] Creation Date:  09-19-2020
+ * 
+ * [] Created By:  Mohammad Mahdi Malmasi (m.mahdi.m79@gmail.com / https://github.com/MmahdiM79)
+ * ==========================================
+ * 
+ * 
+ * This simple code, complie and run the Copier.java file and
+ * Gives the destination path to it
+ *
+ * 
+ * 
+ */
+/*
+ *  Copyright ©️  2020  Mohammad Mahdi Malmasi.
+ */
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
