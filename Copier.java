dd4e259fee946cbf3797f8de97230ece0f777b7f
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-09-19 10:18:39 +0430

    Copier class added

---
+++ Copier.java
@@ -0,0 +1,20 @@
+
+
+
+
+/**
+ * This class copies the songs to the given directory
+ * The default location of songs (iTunes library) in Mac OS is:
+ *      ./Users/<username>/Music/iTunes/iTunes\ Media/Music
+ * 
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public final class Copier
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("test");
+    }
+}
